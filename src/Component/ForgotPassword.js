0c1a0b2b29c58ef35931d9a9ad849b7f473e497c
Arun Kumar <118879115+Arun7398@users.noreply.github.com>
2023-06-29 10:12:49 +0530

    Update ForgotPassword.js

--- src/Component/ForgotPassword.js
+++ src/Component/ForgotPassword.js
@@ -1,41 +1,104 @@
-import React, { useState } from "react";
-import API from "./globalapi";
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useState } from 'react';
+import { useFormik } from "formik";
+import * as yup from "yup";
+import { useNavigate } from "react-router-dom";
+
+
+const userValidationSchema = yup.object({
+    email: yup.string()
+        .required("Email is mandatory"),
+});
+
+const theme = createTheme();
 
 export default function ForgotPassword() {
-  const [email, setEmail] = useState("");
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    const response = await axios.post(
-      `${API}/forgotPassword`,
-      { email: email },
-      { withCredentials: true }
+    const navigate = useNavigate();
+
+    const [user, setUser] = useState({})
+    const formik = useFormik({
+        initialValues: {
+            email: "",
+        },
+        validationSchema: userValidationSchema,
+        onSubmit: (newUser) => {
+            createUser(newUser)
+        }
+    });
+
+    const createUser = (newUser) => {
+        fetch("https://day43-git-master-deepavishali.vercel.app/forgotPassword", {
+            method: "POST",
+            body: JSON.stringify(newUser),
+            headers: { "Content-Type": "application/json" },
+        })
+            .then((res) => {
+                if (res.status === 400) {
+                    alert("Email not registered")
+                }
+                else {
+                    alert("Please check your Email for OTP")
+                    localStorage.setItem("authenticated", true);
+                    navigate("/passwordreset")
+                }
+            })
+            .then((data) => data.json())
+            .then((data) => console.log(data)).catch((err) => console.log(err))
+
+
+        setUser({ ...user, ...newUser });
+    };
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Typography component="h1" variant="h5">
+                        Please type your email Id to reset your Password
+                    </Typography>
+                    <br />
+                    <Box component="form" onSubmit={formik.handleSubmit} noValidate sx={{ mt: 1 }}>
+                        <TextField
+                            id="email"
+                            name="email"
+                            label="email"
+                            variant="outlined"
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            value={formik.values.email}
+                            style={{ width: '500px' }}
+                        />
+                        <br />
+
+                        {formik.touched.email && formik.errors.email ? formik.errors.email : ""}
+                        <br />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Submit
+                        </Button>
+                       </Box>
+                </Box>
+               </Container>
+        </ThemeProvider>
     );
-    alert(response.data.message);
-  };
-  return (
-    <div className="forgot-pass-container">
-      <form>
-        <div className="mb-3">
-          <label htmlFor="email" className=" forgot-email form-label">
-            Enter your email
-          </label>
-          <input
-            type="email"
-            className="form-control"
-            id="email"
-            aria-describedby="emailHelp"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <button
-          type="submit"
-          className="btn btn-primary"
-          onClick={handleSubmit}
-        >
-          Reset password
-        </button>
-      </form>
-    </div>
-  );
 }
