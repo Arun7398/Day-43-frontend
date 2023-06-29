ece82562bdc220e40a7aa5d5f2ba8cab5a0f1647
Arun Kumar <118879115+Arun7398@users.noreply.github.com>
2023-06-29 10:25:31 +0530

    Update ResetPassword.js

--- src/Component/ResetPassword.js
+++ src/Component/ResetPassword.js
@@ -1,61 +1,121 @@
-import React from "react";
-import { useEffect, useState } from "react";
-import API from "./globalapi";
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useState } from 'react';
+import { useFormik } from "formik";
+import * as yup from "yup";
+import { useNavigate } from "react-router-dom";
 
-export default function ResetPassword() {
-  const [userData, setUserData] = useState({
-    userId: "",
-    token: "",
-    password: ""
-  });
-  const navigate = useNavigate();
-  const search = useLocation().search;
-  const token = new URLSearchParams(search).get("token");
-  const userId = new URLSearchParams(search).get("id");
 
-  useEffect(() => {
-    setUserData({ ...userData, userId: userId, token: token });
-  }, []);
+const userValidationSchema = yup.object({
+    secretCode: yup.number()
+        .required("OTP is mandatory"),
+    newPassword: yup.string()
+        .required("Password is required")
+});
 
-  const handleLogin = async (event) => {
-    try {
-      event.preventDefault();
-      const response = await axios.post(`${API}/resetPassword`, userData, {
-        withCredentials: true
-      });
-      if (response) {
-        navigate("/login");
-      }
-    } catch (error) {
-      console.log("Error: ", error);
-    }
-  };
+const theme = createTheme();
 
-  return (
-    <div className="forgot-pass-container">
-      <h3 className="heading">Enter your new password</h3>
-      <form>
-        <div className="mb-3">
-          <label
-            htmlFor="exampleInputPassword1"
-            className="forgot-email form-label"
-          >
-            New Password
-          </label>
-          <input
-            type="password"
-            className="form-control"
-            id="exampleInputPassword1"
-            placeholder="Enter new password"
-            onChange={(e) =>
-              setUserData({ ...userData, password: e.target.value })
-            }
-          />
-        </div>
-        <button type="submit" className="btn btn-primary" onClick={handleLogin}>
-          Reset your password
-        </button>
-      </form>
-    </div>
-  );
+export default function ForgotPassword() {
+
+    const navigate = useNavigate();
+
+    const [user, setUser] = useState({})
+    const formik = useFormik({
+        initialValues: {
+            secretCode: "",
+            newPassword:""
+        },
+        validationSchema: userValidationSchema,
+        onSubmit: (newUser) => {
+            createUser(newUser)
+        }
+    });
+
+    const createUser = (newUser) => {
+        fetch("https://day43-git-master-deepavishali.vercel.app/passwordReset", {
+            method: "POST",
+            body: JSON.stringify(newUser),
+            headers: { "Content-Type": "application/json" },
+        })
+            .then((res) => {
+                if (res.status === 400) {
+                    alert("User does not exist")
+                }
+                else {
+                    alert("Password reset Successful")
+                    localStorage.setItem("authenticated", true);
+                    navigate("/")
+                }
+            })
+            .then((data) => data.json())
+            .then((data) => console.log(data)).catch((err) => console.log(err))
+
+
+        setUser({ ...user, ...newUser });
+    };
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                   
+                    <Typography component="h1" variant="h5">
+                        Please type the OTP received from your Email Id and the New Password
+                    </Typography>
+                    <br />
+                    <Box component="form" onSubmit={formik.handleSubmit} noValidate sx={{ mt: 1 }}>
+                        <TextField
+                            id="secretCode"
+                            name="secretCode"
+                            label="OTP"
+                            variant="outlined"
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            value={formik.values.secretCode}
+                            style={{ width: '500px' }}
+                        />
+                        <br />
+
+                        {formik.touched.secretCode && formik.errors.secretCode ? formik.errors.secretCode : ""}
+                        <br />
+                        <TextField
+              id="newPassword"
+              name="newPassword"
+              label="New password"
+              variant="outlined"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.newPassword}
+              style={{ width: '500px' }}
+            />
+            {formik.touched.newPassword && formik.errors.newPassword ? formik.errors.newPassword : ""}
+            <br />
+
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Submit
+                        </Button>
+                       </Box>
+                </Box>
+                </Container>
+        </ThemeProvider>
+    );
 }
